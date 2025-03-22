96270c8f26dc4744fb7edaaeae67da74c905f956
Guilherme Louback <guilherme_lou@hotmail.com>
2025-03-22 11:58:53 -0300

    refactor: ajustar estilos e layout dos componentes de projetos para melhor responsividade

--- frontend/src/components/projetos/ItemProjeto.tsx
+++ frontend/src/components/projetos/ItemProjeto.tsx
@@ -3,25 +3,22 @@ import Image from "next/image"
 import Link from "next/link"
 
 export interface ItemProjetoProps {
-	projeto: Projeto
+    projeto: Projeto
 }
 
 export default function ItemProjeto(props: ItemProjetoProps) {
-	return (
-		<Link href={`/projeto/${props.projeto.id}`}>
-			<div
-				className="
-                    relative rounded-2xl overflow-hidden border border-red-500
-                    min-w-64 min-h-64 
-                "
-			>
-				<Image
-					src={props.projeto.imagens[0]}
-					alt={props.projeto.nome}
-					fill
-					objectFit="cover"
-				/>
-			</div>
-		</Link>
-	)
+    return (
+        <Link href={`/projeto/${props.projeto.id}`}>
+            <div
+                className="relative rounded-2xl overflow-hidden border border-red-500 w-full sm:w-64 h-64"
+            >
+                <Image
+                    src={props.projeto.imagens[0]}
+                    alt={props.projeto.nome}
+                    fill
+                    objectFit="cover"
+                />
+            </div>
+        </Link>
+    )
 }
--- frontend/src/components/projetos/Readme.tsx
+++ frontend/src/components/projetos/Readme.tsx
@@ -4,11 +4,10 @@ export interface ReadmeProps {
     markdown : string
 }
 
-
 export default function Readme(props: ReadmeProps){
     return(
-        <div className="flex flex-col items-stretch p-6 bg-gray-800 border border-red-500 rounded-2xl">
-            <div className="prose prose-zinc prose-invert" style={{maxWidth: "100%"}}>
+        <div className="flex flex-col items-stretch p-6 bg-gray-800 border border-red-500 rounded-2xl w-full max-w-screen-md mx-auto">
+            <div className="prose prose-zinc prose-invert w-full" style={{ maxWidth: "100%" }}>
                 <ConteudoMD markdown={props.markdown}/>
             </div>
         </div>
