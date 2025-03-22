a6431111f3eec45b300e98722bba920c16f76c26
Guilherme Louback <guilherme_lou@hotmail.com>
2025-03-22 11:03:56 -0300

    refactor: ajustar a função PaginaProjeto para utilizar Promise no parâmetro e melhorar a legibilidade do código

--- frontend/src/app/(paginas)/projeto/[id]/page.tsx
+++ frontend/src/app/(paginas)/projeto/[id]/page.tsx
@@ -1,29 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import Cabecalho from "@/components/shared/Cabecalho"
 import { obterProjeto } from "@/functions/projetos"
 import Container from "@/components/shared/Container"
 import CarrosselImagens from "@/components/shared/CarroselImagens"
 import Tecnologias from "@/components/tecnologias/tecnologias"
 import { obterReadme } from "@/functions/github"
-import ConteudoMD from "@/components/shared/ConteudoMD"
 import Readme from "@/components/projetos/Readme"
 
-export default async function PaginaProjeto(props: { params: { id: string } }) {
-    const { id } = await props.params
-    const projeto = await obterProjeto(id)
+export default async function PaginaProjeto(props: { params: Promise<{ id: string }> }) {
+	const { id } = await props.params
+	const projeto = await obterProjeto(id)
 
-    if(!projeto) return null
-    const readme = await obterReadme(projeto.repositorio)
+	if (!projeto) return null
 
-    return (
-        <div className="bg-black">
-            <Cabecalho />
-            <Container className="py-7 flex flex-col items-center gap-10">
-                <h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
-                <CarrosselImagens imagens={projeto.imagens.slice(1)} />
-                <Tecnologias lista={projeto.tecnologias} tamanhoMenor />
-                <Readme markdown={readme} />
-            </Container>
-        </div>
-    )
+	const readme = await obterReadme(projeto.repositorio)
+
+	return (
+		<div className="bg-black">
+			<Cabecalho />
+			<Container className="py-7 flex flex-col items-center gap-10">
+				<h1 className="text-3xl font-bold self-start">{projeto.nome}</h1>
+				<CarrosselImagens imagens={projeto.imagens.slice(1)} />
+				<Tecnologias lista={projeto.tecnologias} tamanhoMenor />
+				<Readme markdown={readme} />
+			</Container>
+		</div>
+	)
 }
